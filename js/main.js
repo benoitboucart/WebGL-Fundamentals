e8c33527eab92062e48bb38573b3d35dfe4e7794
Benoît Boucart <benoit@webbb.be>
2018-09-04 10:12:34 +0200

    Draw 50 rectangles

--- js/main.js
+++ js/main.js
@@ -27,13 +27,42 @@
     // fragment shaders don't have a default precision so we need
     // to pick one. mediump is a good default. It means "medium precision"
     precision mediump float;
-    
+    uniform vec4 u_color;
+
     void main() {
       // gl_FragColor is a special variable a fragment shader
       // is responsible for setting
-      gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+      // gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+      gl_FragColor = u_color;
     }
   `;
+
+  // Returns a random integer from 0 to range - 1.
+  const randomInt = (range) => {
+    return Math.floor(Math.random() * range);
+  }
+
+  // Fills the buffer with the values that define a rectangle.
+  const setRectangle = (gl, x, y, width, height) => {
+    const x1 = x;
+    const x2 = x + width;
+    const y1 = y;
+    const y2 = y + height;
+
+    // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+    // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+    // but so far we only have one buffer. If we had more than one
+    // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      x1, y1,
+      x2, y1,
+      x1, y2,
+      x1, y2,
+      x2, y1,
+      x2, y2
+    ]), gl.STATIC_DRAW);
+  }
+
 
   const createShader = (gl, type, source) => {
     const shader = gl.createShader(type);
@@ -83,8 +112,10 @@
     // a_position which is an attribute van de vertexShaderSource
     // look up where the vertex data needs to go.
     const positionAttributeLocation = gl.getAttribLocation(program, `a_position`);
-
+    
+    // look up uniform locations
     const resolutionUniformLocation = gl.getUniformLocation(program, `u_resolution`);
+    const colorUniformLocation = gl.getUniformLocation(program, "u_color");
 
     // Attributes get their data from buffers so we need to create a buffer
     // You can think of bind points as internal global variables inside WebGL
@@ -94,20 +125,7 @@
     // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    // Now we can put data in that buffer by referencing it through the bind point
-    // three 2d points (JS array)
-    var positions = [
-      // x, y are set z & w will be default 0 and 1
-      // draw a rectangle made from 2 triangles
-      10, 20,
-      80, 20,
-      10, 30,
-      10, 30,
-      80, 20,
-      80, 30,
-    ];
-    // WebGL = strongly typed: JS array omzetten naar 32bit floating point 
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    
 
     // code above this line is initialization code.
     // code below this line is rendering code.
@@ -144,14 +162,55 @@
     
     // set the resolution
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+    
+    /**
+     * draw 1 rectangle
+     */
+    // Now we can put data in that buffer by referencing it through the bind point
+    // 6 2d points (JS array): 2 triangles to draw a rectangle
+    var positions = [
+      // x, y are set z & w will be default 0 and 1
+      // draw a rectangle made from 2 triangles
+      10, 20,
+      80, 20,
+      10, 30,
+      10, 30,
+      80, 20,
+      80, 30,
+    ];
+    // WebGL = strongly typed: JS array omzetten naar 32bit floating point 
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-    // draw
     const primitiveType = gl.TRIANGLES;
     offset = 0;
     // will execute our vertex shader COUNT times (3 for a triangle)
     // first time: a_position.x & y will be 2 first values, 2nd time 2nd two values etc
     const count = 6;
+    // Random color
+    gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
     gl.drawArrays(primitiveType, offset, count);
+
+    /**
+     * Draw 50 random rectangles in random colors
+     */
+    for (let ii = 0; ii < 50; ++ii) {
+      // Setup a random rectangle
+      // This will write to positionBuffer because
+      // its the last thing we bound on the ARRAY_BUFFER
+      // bind point
+      setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+
+      // Set a random color.
+      gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+      // Draw the rectangle.
+      const primitiveType = gl.TRIANGLES;
+      const offset = 0;
+      // will execute our vertex shader COUNT times (3 for a single triangle)
+      // first time: a_position.x & y will be 2 first values, 2nd time 2nd two values etc
+      const count = 6;
+      gl.drawArrays(primitiveType, offset, count);
+    }
   }
 
   init();
