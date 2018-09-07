43bc320ada262333c7efb4301c63443e11c2db3a
Benoît Boucart <benoit@webbb.be>
2018-09-07 14:29:10 +0200

    remove projection

--- draw-rectangle-9-matrices-with-projection/js/main.js
+++ draw-rectangle-9-matrices-with-projection/js/main.js
@@ -9,15 +9,6 @@
       ];
     },
 
-    projection: (width, height) => {
-      // Note: This matrix flips the Y axis so that 0 is at the top.
-      return [
-        2 / width, 0, 0,
-        0, -2 / height, 0,
-        -1, 1, 1
-      ];
-    },
-    
     translation: (tx, ty) => {
       return [
         1, 0, 0,
